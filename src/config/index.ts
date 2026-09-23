5c80fd17c6ed6ac7826fa761d8cbe665f30f27f7
Oscar Llamuca <oscarandre998@gmail.com>
2026-09-23 14:41:50 -0500

    feat: Cargas de archivos simple y multiple y obtención de archivos mediante http

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,4 +2,5 @@ export * from './bcrypt.adapter';
 export * from './envs';
 export * from './regular-exp';
 export * from './jwt.adapter';
-export  * from './validators';
+export * from './validators';
+export * from './uuid.adapter';
